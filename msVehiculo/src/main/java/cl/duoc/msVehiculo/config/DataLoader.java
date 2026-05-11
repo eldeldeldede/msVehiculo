89bd292fe8ab2fe1d12cd1ba8e802357a3c2ec44
jordyhrnlzn <101402897+jordyhrnlzn@users.noreply.github.com>
2026-05-11 12:25:31 -0400

    DataLoader creado

---
+++ msVehiculo/src/main/java/cl/duoc/msVehiculo/config/DataLoader.java
@@ -0,0 +1,68 @@
+package cl.duoc.msVehiculo.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import cl.duoc.msVehiculo.model.Marca;
+import cl.duoc.msVehiculo.model.Modelo;
+import cl.duoc.msVehiculo.model.TipoVehiculo;
+import cl.duoc.msVehiculo.model.Vehiculo;
+import cl.duoc.msVehiculo.repository.MarcaRepository;
+import cl.duoc.msVehiculo.repository.ModeloRepository;
+import cl.duoc.msVehiculo.repository.TipoVehiculoRepository;
+import cl.duoc.msVehiculo.repository.VehiculoRepository;
+
+@Configuration
+public class DataLoader {
+    @Bean
+    CommandLineRunner initDataBase(VehiculoRepository vehiculoRepo,
+                                   MarcaRepository marcaRepo,
+                                   TipoVehiculoRepository tipoRepo,
+                                   ModeloRepository modeloRepo){
+                                    return args -> {
+
+
+                                        if(marcaRepo.count() > 0){
+                                            System.out.println("No se cargó nada porque ya hay datos");
+                                        }else{
+                                            Marca marca1 = new Marca(null, "KIA", "Corea del Sur");
+                                            Marca marca2 = new Marca(null, "Hyundai", "Corea del Sur");
+                                            Marca marca3 = new Marca(null, "Mitsubishi", "Japón");
+
+                                            marcaRepo.save(marca1);
+                                            marcaRepo.save(marca2);
+                                            marcaRepo.save(marca3);
+                                        
+                                            Modelo modelo1 = new Modelo(null, "Morning", 2018, 5, 5, "MECANICA", "93 OCTANOS", marca1);
+                                            Modelo modelo2 = new Modelo(null, "Accent", 2018, 5, 5, "MECANICA", "93 OCTANOS", marca2);
+                                            Modelo modelo3 = new Modelo(null, "Lancer X", 2015, 5, 5, "MECANICA", "93 OCTANOS", marca3);
+
+                                            modeloRepo.save(modelo1);
+                                            modeloRepo.save(modelo2);
+                                            modeloRepo.save(modelo3);
+
+                                            TipoVehiculo tipo1 = new TipoVehiculo(null,"Sedán");
+                                            TipoVehiculo tipo2 = new TipoVehiculo(null,"City car");
+                                            TipoVehiculo tipo3 = new TipoVehiculo(null,"SUV");
+
+                                            tipoRepo.save(tipo1);
+                                            tipoRepo.save(tipo2);
+                                            tipoRepo.save(tipo3);
+
+                                            Vehiculo vehiculo1 = new Vehiculo(null, "AA-0000", modelo1, 0, "Negro", tipo2);
+                                            Vehiculo vehiculo2 = new Vehiculo(null, "BB-0000", modelo2, 0, "Negro", tipo1);
+                                            Vehiculo vehiculo3 = new Vehiculo(null, "CC-0000", modelo3, 0, "Negro", tipo1);
+
+                                            vehiculoRepo.save(vehiculo1);
+                                            vehiculoRepo.save(vehiculo2);
+                                            vehiculoRepo.save(vehiculo3);
+
+
+                                            System.out.println("Datos cargados con éxito a la bd!");
+                                        }
+                                    };
+
+                                   }
+    
+}
