7605af47c5e654ef48c808da858af14309f1e89a
Pedro420-z <pedro.alejandrocm2006@gmail.com>
2026-06-23 15:37:03 -0400

    Add Schema annotations to Vehiculo fields

--- msVehiculo/src/main/java/cl/duoc/msVehiculo/model/Vehiculo.java
+++ msVehiculo/src/main/java/cl/duoc/msVehiculo/model/Vehiculo.java
@@ -23,26 +23,34 @@ public class Vehiculo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description="ID unico con el cual se identifica el vehiculo a arrendar")
     private Integer id;
 
     @Column(nullable = false, unique = true)
+    @Schema(description="Patente o matricula con la cual se identifica el vehiculo a arrendar")
     private String patente;
 
     @ManyToOne
     @JoinColumn(name = "modelo_id")
+    @Schema(description="Modelo del vehiculo a arrendar")
     private Modelo modelo;
     
     @Column(nullable = false)
+    @Schema(description="cantidad de kilometraje que recorrió el vehiculo anteriormente")
     private Integer kilometraje;
 
     @Column(nullable = false)
+    @Schema(description="color del vehiculo a arrendar")
     private String color;
 
     @Column(name = "sucursal_id", nullable = false)
+    @Schema(description="ID de la sucursal en la cual se solicitó el vehiculo")
     private Integer sucursalId;
 
     @ManyToOne
     @JoinColumn(name = "tipovehiculo_id", nullable = false)
     @JsonBackReference
+    @Schema(description="tipo de vehiculo que se va a arrendar",
+            examples={"SUV, hatchback, pickup, station wagon"})
     private TipoVehiculo tipoVehiculo; 
 }
